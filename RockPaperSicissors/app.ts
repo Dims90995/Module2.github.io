d27d2f7bc8e996ee70413d5de388de6c6f75e76b
Dima Shamis <dims90995@gmail.com>
2025-02-24 17:19:23 +0200

    UPDATE

---
+++ RockPaperSicissors/app.ts
@@ -0,0 +1,35 @@
+
+
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay") as HTMLDivElement;
+const computerDisplay = document.getElementById("computerDisplay") as HTMLDivElement;
+const resultDisplay = document.getElementById("resultDisplay") as HTMLDivElement;
+
+function playGame(playerChoice){
+
+const computerChoice = choices[Math.floor(Math.random() * 3)];
+let result = "";
+
+if(playerChoice === computerChoice){
+    result = "It's a tie!";
+}
+else{
+    switch(playerChoice){
+        case "rock":
+            result = (computerChoice === "scissors") ?  "You win!" :  "You lose!";
+            break;
+            case "paper":
+                result = (computerChoice === "rock") ?  "You win!" :  "You lose!";
+                break;
+                case "scissors":
+                    result = (computerChoice === "paper") ?  "You win!" :  "You lose!";
+                    break;
+    }
+}
+
+playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+computerDisplay.textContent = `Computer: ${computerChoice}`;
+resultDisplay.textContent = result;
+
+
+}
---
+++ RockPaperSicissors/dist/app.js
@@ -0,0 +1,27 @@
+var choices = ["rock", "paper", "scissors"];
+var playerDisplay = document.getElementById("playerDisplay");
+var computerDisplay = document.getElementById("computerDisplay");
+var resultDisplay = document.getElementById("resultDisplay");
+function playGame(playerChoice) {
+    var computerChoice = choices[Math.floor(Math.random() * 3)];
+    var result = "";
+    if (playerChoice === computerChoice) {
+        result = "It's a tie!";
+    }
+    else {
+        switch (playerChoice) {
+            case "rock":
+                result = (computerChoice === "scissors") ? "You win!" : "You lose!";
+                break;
+            case "paper":
+                result = (computerChoice === "rock") ? "You win!" : "You lose!";
+                break;
+            case "scissors":
+                result = (computerChoice === "paper") ? "You win!" : "You lose!";
+                break;
+        }
+    }
+    playerDisplay.textContent = "PLAYER: " + playerChoice;
+    computerDisplay.textContent = "Computer: " + computerChoice;
+    resultDisplay.textContent = result;
+}
