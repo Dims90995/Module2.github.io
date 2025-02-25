50a980ff90a6734e1aece69c1326817487a4437e
Dima Shamis <dims90995@gmail.com>
2025-02-25 18:15:24 +0200

    home page update

---
+++ Homepage.html/dist/dist/clock.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var clockElement = document.getElementById('clock');
+
+function updateClock() {
+  var now = new Date();
+  var hours = now.getHours().toString().padStart(2, '0');
+  var minutes = now.getMinutes().toString().padStart(2, '0');
+  var seconds = now.getSeconds().toString().padStart(2, '0');
+  var timeString = hours + ":" + minutes + ":" + seconds;
+
+  if (clockElement) {
+    clockElement.textContent = timeString;
+  }
+}
+
+updateClock();
+setInterval(updateClock, 1000);
